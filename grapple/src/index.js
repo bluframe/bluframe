694525808a7aa81804619cff6d78b1cd9da11512
Edoardo L'Astorina <edoardo@blufra.me>
2022-10-15 01:47:12 +0100

    Hotfix: Grapple: useToggle

--- grapple/src/index.js
+++ grapple/src/index.js
@@ -3,4 +3,5 @@
 export { default as prepareComponent } from "./prepareComponent"
 export { default as useOnScreen } from "./useOnScreen"
 export { default as usePrevious } from "./usePrevious"
+export { default as useToggle } from "./useToggle"
 export { default as useTrimText } from "./useTrimText"
