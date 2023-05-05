2a445d7e44ea9601be3993ea0c22fdd1ca61e972
Edoardo L'Astorina <edoardo@blufra.me>
2023-05-05 13:49:04 +0100

    Shrink Label

--- blublocks/src/components/inputs/InputText/InputText.js
+++ blublocks/src/components/inputs/InputText/InputText.js
@@ -2,28 +2,43 @@
 /* eslint-disable no-ternary, react/jsx-no-bind */
 
 import { Input, Label, Wrapper } from "./styled"
+import React, { useState } from "react"
 import type { ComponentProps } from "."
-import React from "react"
 
-const InputText = ({
+const TextInput = ({
   inputId,
   label,
   labelId,
   name,
   onChange,
   value
-}: ComponentProps): React$Node => (
-  <Wrapper>
-    <Label htmlFor={inputId} id={labelId}>
-      {label}
-    </Label>
-    <Input
-      id={inputId}
-      name={name}
-      onChange={(event) => onChange(event.target.value)}
-      value={value}
-    />
-  </Wrapper>
-)
+}: ComponentProps): React$Node => {
+  const [isFocused, setIsFocused] = useState(false)
 
-export default InputText
+  const handleFocus = () => {
+    setIsFocused(true)
+  }
+
+  const handleBlur = () => {
+    setIsFocused(false)
+  }
+
+  return (
+    <Wrapper>
+      <Input
+        id={inputId}
+        isFocused={isFocused}
+        name={name}
+        onBlur={handleBlur}
+        onChange={(event) => onChange(event.target.value)}
+        onFocus={handleFocus}
+        value={value}
+      />
+      <Label htmlFor={inputId} id={labelId} isFocused={isFocused}>
+        {label}
+      </Label>
+    </Wrapper>
+  )
+}
+
+export default TextInput
--- blublocks/src/components/inputs/InputText/styled.js
+++ blublocks/src/components/inputs/InputText/styled.js
@@ -7,26 +7,47 @@ import type { Theme } from "@bluframe/blublocks"
 export const Wrapper: StyledComponent<{||}, Theme, HTMLDivElement> = styled.div`
   display: flex;
   flex-direction: column;
+  position: relative;
+  width: 100%;
 `
 
 export const Input: StyledComponent<
-  {||},
+  {| +isFocused: boolean |},
   Theme,
   HTMLInputElement
 > = styled.input.attrs({ type: "text" })`
   border: 1px solid ${({ theme }) => theme.palette.primary.main};
   border-radius: 4px;
   font-size: 16px;
-  padding: 8px;
+  padding: 12px 8px;
   width: 100%;
+
+  &:focus + label {
+    color: ${({ theme }) => theme.palette.primary.main};
+    font-size: 12px;
+    top: 4px;
+  }
+
+  ${({ isFocused, value }) =>
+    (isFocused || value) &&
+    `
+    & + label {
+      font-size: 12px;
+      top: 4px;
+    }
+  `}
 `
 
 export const Label: StyledComponent<
-  {||},
+  {| +isFocused: boolean |},
   Theme,
   HTMLLabelElement
 > = styled.label`
   color: ${({ theme }) => theme.palette.text.main};
   font-size: 16px;
-  margin-bottom: 4px;
+  left: 8px;
+  pointer-events: none;
+  position: absolute;
+  top: 12px;
+  transition: all 200ms;
 `
