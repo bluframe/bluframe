6c4b2800a8abe8ffcac77e86b60c0dd213218493
Edoardo L'Astorina <edoardo@blufra.me>
2023-05-08 02:41:07 +0100

    BLUFRAMEOS-27 PrepareComponent forwardRef (#30)

--- grapple/src/prepareComponent.js
+++ grapple/src/prepareComponent.js
@@ -5,17 +5,15 @@ import type {
   Prepare,
   PrepareComponentOptions
 } from "@bluframe/grapple"
-import React, { useMemo } from "react"
+import React, { type ElementRef, forwardRef, useMemo } from "react"
 
 /*
- *
  * Receive Props, transform them in ComponentProps and return
  * Component with ComponentProps
  *
  * onlyRenderIf: only render component if function returns true
  *
  * onlyUpdate: only update component if a prop in array changes
- *
  */
 const prepareComponent =
   <Props: {| +[string]: any |}, ComponentProps: {| +[string]: any |}>(
@@ -25,28 +23,38 @@ const prepareComponent =
   (
     Component: React$AbstractComponent<ComponentProps, any>
   ): React$AbstractComponent<Props, any> => {
-    const PrepareComponent = (props: Props): React$Node => {
-      const newProps: ComponentProps = usePrepareComponent(props)
+    // PrepareComponent is a higher-order component that wraps the original Component
+    // and handles the props transformation, rendering, and updating logic.
+    const PrepareComponent = forwardRef(
+      (props: Props, ref: ElementRef<any>): React$Node => {
+        // Transform Props into ComponentProps using the usePrepareComponent function
+        const newProps: ComponentProps = usePrepareComponent(props)
 
-      let updateProps = [newProps]
+        // Determine which props to use for controlling component updates
+        let updateProps = [newProps]
+        if (options && options.onlyUpdate) {
+          updateProps = options.onlyUpdate.map((item) => props[item])
+        }
 
-      if (options && options.onlyUpdate) {
-        // eslint-disable-next-line react/destructuring-assignment
-        updateProps = options.onlyUpdate.map((item) => props[item])
+        // MemoizeComponent is a memoized version of the Component that only updates when
+        // the specified props change, as defined by the onlyUpdate option.
+        const MemoizeComponent = useMemo(
+          () => <Component {...newProps} ref={ref} />,
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          updateProps
+        )
+
+        // Only render the component if the onlyRenderIf option is not provided or
+        // if it returns true.
+        if (options && options.onlyRenderIf && !options.onlyRenderIf(props)) {
+          return null
+        }
+
+        return MemoizeComponent
       }
+    )
 
-      const MemoizeComponent = useMemo(
-        () => <Component {...newProps} />,
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        updateProps
-      )
-
-      if (options && options.onlyRenderIf && !options.onlyRenderIf(props)) {
-        return null
-      }
-
-      return MemoizeComponent
-    }
+    PrepareComponent.displayName = "PrepareComponent"
 
     return PrepareComponent
   }
