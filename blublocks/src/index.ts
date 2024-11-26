bd587cd1f04da60622a2f12b4ad1a925d5660c43
Edoardo L'Astorina <edoardo@blufra.me>
2024-11-26 12:46:48 +0000

    🔧 Enhanced ESLint configuration and updated changelog
    Added TypeScript ESLint plugin to the ESLint configuration for better TypeScript linting. Updated the changelog with detailed version history, including minor and patch changes. Removed unnecessary Jest transformer files for CSS and file imports, simplifying the testing setup.

--- blublocks/src/index.ts
+++ blublocks/src/index.ts
@@ -10,6 +10,19 @@ export {
   default as Button
 } from "components/buttons/Button"
 export {
+  type Props as BreadcrumbsProps,
+  default as Breadcrumbs
+} from "components/Breadcrumbs"
+export { default as Card, type Props as CardProps } from "components/Cards/Card"
+export {
+  default as Cards,
+  type Props as CardsProps
+} from "components/Cards/Cards"
+export {
+  type Props as CarouselProps,
+  default as Carousel
+} from "components/Carousel"
+export {
   type OptionValue,
   default as Checkbox
 } from "components/inputs/Checkbox"
@@ -23,6 +36,7 @@ export { default as DeleteButton } from "components/buttons/DeleteButton"
 export { default as EditButton } from "components/buttons/EditButton"
 export { default as FadeIn } from "components/animations/FadeIn"
 export { default as Flip } from "components/animations/Flip"
+export { default as FormModal } from "components/modals/FormModal"
 export { default as H1 } from "components/Typography/H1"
 export { default as H2 } from "components/Typography/H2"
 export { default as H3 } from "components/Typography/H3"
@@ -36,22 +50,31 @@ export {
   type Props as InputTextProps,
   default as InputText
 } from "components/inputs/InputText"
+export { default as Link } from "components/Typography/Link"
 export { default as Loading } from "components/Loading"
 export {
   type OnSubmitLogin,
   type Props as LoginProps,
   default as Login
 } from "components/Login"
-export { default as Modal } from "components/modals/Modal"
+export {
+  default as Modal,
+  type Props as ModalProps
+} from "components/modals/Modal"
 export { default as Paragraph } from "components/Typography/Paragraph"
 export { default as Radio } from "components/inputs/Radio"
 export { default as RadioGroup } from "components/inputs/RadioGroup"
+export { default as Reset } from "components/Reset"
 export { default as ResetButton } from "components/buttons/Reset"
 export {
   type Option,
   type Props as SelectProps,
   default as Select
 } from "components/inputs/Select"
+export {
+  default as SocialLinks,
+  type Props as SocialLinksProps
+} from "components/SocialLinks"
 export { default as SubtitleH1 } from "components/Typography/SubtitleH1"
 export { default as SubtitleH2 } from "components/Typography/SubtitleH2"
 export { default as SubtitleH3 } from "components/Typography/SubtitleH3"
